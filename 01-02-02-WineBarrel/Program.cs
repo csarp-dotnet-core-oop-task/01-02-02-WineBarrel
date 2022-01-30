a1425aca9944d1ff2c97d762ef73db2d6bf0900c
gycsaba <gyuris.csaba@vasvari.org>
2022-01-30 18:44:38 +0100

    Kész feladat

---
+++ 01-02-02-WineBarrel/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+A feladat boros pince hordóival kapcsolatos. Készítsen egy boros hordó (wine barrel) osztályt amelyben eltárolható a bor neve (szöveg) és a bor mennyisége a hordóban (egész szám). A boroshordó a benne lévő bor megnevezésének és a benne lévő bor mennyiségének a megadásával jöhet létre!
+Boroshordó létrehozásakor a felhasználó negatív számot is megadhat a mennyiség paraméternek. Ilyenkor a boroshordó legyen üres!
+ A boros hordó osztályhoz biztosítson egy metódust, amely segítségével a gazda adott mennyiségű bort vehet ki a hordóból (take out wine). A gazda csak annyi bort vehet ki a hordóból amennyi abban benne van, vagy annál kevesebbet!
+A hordóban lévő bor mennyiségének ellenőrzésére írnia kell egy metódus, amely kiírja a játékos nevét és koordinátáját. A metódus neve legyen „ToConsole”.
+Pl. ha a bor neve neve „Egri leányka”, és a és a hordóban lévő bor mennyisége 100 liter, akkor a kiírást egy új sorral záruljon és a következő formátumba jelenjen meg:
+
+Egri leányka mennyisége a hordóban 100 liter.
+
+Figyeljen oda, hogyha már elfogyott a hordóban a bor és ki akarnak venni belőle bort, akkor írja ki a képernyőre a ToConsole metódus a következő üzenetet: „A hordó üres!”
+Ellenőrzésként vegye fel Boros Béla olaszrizlinget tartalmazó hordóját, amelyben 60 liter bot van, és mutassa be, hogyan fogyott el a bor a hordóból! Egyszer próbáljon meg több bort kivenni a hordóból, mint amennyi abban van!
+Dokumentálja az elkészült osztályt UML osztálydiagrammal!
+*/
+
+using System;
+
+namespace _01_02_02_WineBarrel
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- tests/Tests.cs
+++ tests/Tests.cs
@@ -6,15 +6,130 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace gyak_01_02_01_Maze.Tests
+using _01_02_02_WineBarrel;
+
+namespace Test.Tests
 {
     [TestClass()]
     public class Tests
     {
         [TestMethod()]
-        public void Test()
+        public void TestKonstrktor()
         {
-            Assert.Fail();
+            string expected=string.Empty;
+            string actual = "somthing";
+            try
+            {
+                WineBarrel wb = new WineBarrel("Egri leányka", 10);
+                 expected = "Egri leányka mennyisége a hordóban 10 liter.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                wb.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("WineBarrel osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A WineBarrel->Konstruktor nem tárolja el a paraméterben lévő adatokat vagy a WineBarrel->ToConsole nem megfelően működik.");
         }
-   }
+
+        [TestMethod()]
+        public void TestKonstrktorNegativLiter()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                WineBarrel wb = new WineBarrel("Egri leányka", -10);
+                expected = "A hordó üres!\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                wb.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("WineBarrel osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A WineBarrel->Konstruktor negatív értékű liter esetén helytelenül működik vagy a WineBarrel->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorTakeOutWinePossible()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                WineBarrel wb = new WineBarrel("Egri leányka", 100);
+                wb.takeOutWine(80);
+                expected = "Egri leányka mennyisége a hordóban 20 liter.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                wb.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("WineBarrel osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A WineBarrel->TakeOutWine metódus amikor lehetséges a bor mennyiség kivétele nem jól működik vagy a WineBarrel->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorTakeOutWineNotPossible()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                WineBarrel wb = new WineBarrel("Egri leányka", 100);
+                wb.takeOutWine(180);
+                expected = "Egri leányka mennyisége a hordóban 100 liter.\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                wb.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("WineBarrel osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A WineBarrel->TakeOutWine metódus amikor a megadott bor mennyiség kivétele nem lehetséges nem jól működik vagy a WineBarrel->ToConsole nem megfelően működik.");
+        }
+
+        [TestMethod()]
+        public void TestKonstrktorBarrelIsEmpty()
+        {
+            string expected = string.Empty;
+            string actual = "somthing";
+            try
+            {
+                WineBarrel wb = new WineBarrel("Egri leányka", 100);
+                wb.takeOutWine(20);
+                wb.takeOutWine(80);
+                expected = "A hordó üres!\r\n";
+
+                StringWriter sw = new StringWriter();
+                Console.SetOut(sw);
+                wb.ToConsole();
+                actual = sw.ToString();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("WineBarrel osztály kivételt dob!");
+            }
+
+            Assert.AreEqual(expected, actual, "A WineBarrel->ToConsole metódus amikor az összes bort kivették nem megfelelően működik.");
+        }
+    }
 }
